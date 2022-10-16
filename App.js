a07be2ad0797af562a77e4c50bdef817beebdf6a
James Ma <t90laser93@gmail.com>
2022-10-16 17:16:48 +0900

    REFACTOR: SignUpScreen 컴포넌트 로직 분리(BasicUserDataInputForm)

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
+import React from "react";
 import "react-native-gesture-handler";
-import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
 import Main from "./Screens/Main/Main"; // 메인 화면
 import LocalSettingFirst from "./Screens/Main/LocalSetting/LocalSettingFirst";
 import LoginScreen from "./Screens/Login/LoginScreen"; // 학번로그인 일경우 학번 로그인 화면으로 전환
-import SignUpScreen from "./Screens/Login/SignUpScreen"; // 회원가입 페이지
+import SignUpScreen from "./Screens/Login/SignUpScreen"; // 회원가입
 import TicketScreen from "./Screens/Ticket/TicketScreen";
 import BordingList from "./Screens/List/BordingList";
 import DiclationScreen from "./Screens/Diclation/DiclationScreen";
@@ -15,7 +16,6 @@ import PassengerBordingList from "./Screens/List/PassengerBordingList";
 import ProfileScreen from "./Screens/Profile/ProfileScreen";
 import ProfileUpdateScreen from "./Screens/Profile/ProfileUpdateScreen";
 import Setting from "./Screens/Setting/Setting";
-import axios from "axios";
 import InquiryScreen from "./Screens/Inquiry/InquiryScreen";
 import Inquiry from "./Screens/Inquiry/Inquiry";
 
